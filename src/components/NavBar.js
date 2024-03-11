3759f50958eb8ae8ca11fb4b2e07dcd5e4f8372a
NicoSem <nicolae.semionov@gmail.com>
2024-03-11 12:56:02 -0400

    Removed generated code

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    About Me
+                </li>
+                <li>
+                    Projects
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </nav>
+    )
+}
