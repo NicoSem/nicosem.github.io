a28d00ef950799ffcf626c874c8815ffe43dd346
NicoSem <nicolae.semionov@gmail.com>
2024-03-11 23:43:42 -0400

    created individual project pages

--- src/pages/ProjectDetailsPage.js
+++ src/pages/ProjectDetailsPage.js
@@ -1,10 +1,15 @@
 import { useParams } from "react-router-dom";
+import projects from "../components/ProjectDetails";
 
 const ProjectDetailsPage = () => {
     const { projectId } = useParams()
+    const project = projects.find(project => project.name === projectId)
 
     return (
-        <h1>This is the project page for {projectId}!</h1>
+        <>
+            <h1>{project.name}</h1>
+            <p>{project.description}</p>
+        </>
     )
 }
 
