4a72e2274416e9fe5713cfb981ebe1f83d0a4130
NicoSem <nicolae.semionov@gmail.com>
2024-03-11 22:11:29 -0400

    Added projectId param

--- src/pages/ProjectDetailsPage.js
+++ src/pages/ProjectDetailsPage.js
@@ -1,6 +1,10 @@
+import { useParams } from "react-router-dom";
+
 const ProjectDetailsPage = () => {
+    const { projectId } = useParams()
+
     return (
-        <h1>This is the project page</h1>
+        <h1>This is the project page for {projectId}!</h1>
     )
 }
 
