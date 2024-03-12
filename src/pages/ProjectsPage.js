d73adb2fe1e709865b891d67416cd2aa406de5fa
NicoSem <nicolae.semionov@gmail.com>
2024-03-11 23:50:17 -0400

    added projects page

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom"
+import projects from "../components/ProjectDetails"
+
 const ProjectsPage = () => {
     return (
-        <h1>This is the projects page</h1>
+        <>
+            <h1>Projects</h1>
+            {projects.map(project => (
+                <Link to={`/projects/${project.name}`}>
+                    <h3>{project.name}</h3>
+                    <p>{project.description}</p>
+                </Link>
+            ))}
+        </>
+        
     )
 }
 
