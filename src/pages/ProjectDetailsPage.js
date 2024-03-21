4c0e0c3fa5a5cf02d1e60a848b039543cdbbe622
NicoSem <nicolae.semionov@gmail.com>
2024-03-21 19:05:46 -0400

    project styling

--- src/pages/ProjectDetailsPage.js
+++ src/pages/ProjectDetailsPage.js
@@ -1,5 +1,7 @@
+import "./ProjectDetailsPage.scss"
 import { useParams } from "react-router-dom";
 import projects from "../components/ProjectDetails";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const ProjectDetailsPage = () => {
     const { projectId } = useParams()
@@ -7,7 +9,11 @@ const ProjectDetailsPage = () => {
 
     return (
         <>
-            <h1>{project.title}</h1>
+            <div className="project-header">
+                <h1>{project.title}</h1>
+                <a href={project.github}><GitHubIcon/></a>
+            </div>
+            
             <p>{project.description}</p>
         </>
     )
